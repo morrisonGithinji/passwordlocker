f9a0201a34e07383bf912a54eb24403e9572fadc
morrisonGithinji <morrison.githinji@student.moringaschool.com>
2021-10-25 11:39:25 +0300

    add test for user and accounts

--- user_test.py
+++ user_test.py
@@ -19,7 +19,23 @@ class TestUser(unittest.TestCase):
 
         self.assertEqual(self.new_user.userName,"morrison")
         self.assertEqual(self.new_user.password,"5430")
-       
+
+
+       def test_save_user(self):
+        '''
+        test_save_user test case to test if the contact object is saved into
+         the user list
+        '''
+        self.new_user.save_user() # saving the new user
+        self.assertEqual(len(User.user_list),1)
+
+        def test_account_exist(self):
+          self.new_user.save_user()
+          test_user =User("charles","wayhome")
+          test_user.save_user
+          user_exists -User.user_exists("charles")
+
+          self.assertTrue(user_exists)
 
 
 if __name__ == '__main__':
