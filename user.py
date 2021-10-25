0a7c898cb8311f5616fb635d2c043c37105e0799
morrisonGithinji <morrison.githinji@student.moringaschool.com>
2021-10-25 10:07:38 +0300

    create user class

---
+++ user.py
@@ -0,0 +1,12 @@
+class User :
+
+  def __init__(self,first_name,last_name,phone_number,email):
+
+      '''
+      defining properities in the class user
+      '''
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = number
+        self.email = email
