45e11449567c83bd6fae8afd666dcdd7a68c84c6
morrisonGithinji <morrison.githinji@student.moringaschool.com>
2021-10-25 10:40:09 +0300

    add credential properties

--- credentials.py
+++ credentials.py
@@ -5,4 +5,8 @@ class Credentials:
   """
   class having user credentials
   """
-  
+  credentials_list=[]
+  def __init__(self,account,user_id,usercode):
+    self.account =account
+    self.user =user
+    self.usercode =usercode
