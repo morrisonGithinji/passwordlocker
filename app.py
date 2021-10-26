28e3cc933127cca2af3e2e202be0f8008e76ccb2
morrisonGithinji <morrison.githinji@student.moringaschool.com>
2021-10-26 10:23:39 +0300

    add function to delete credentias

--- app.py
+++ app.py
@@ -2,14 +2,33 @@ from user import User
 from credentials import Credentials
 
 def create_user(userName,password):
-  """
+   """
   creating a new password locker account.
   """
   new_user =User(userName,password)
   return new_user
 
-  def save_user(user):
-    """
+def save_user(user):
+     """
      save user
      """
      user.save_user()
+
+def create_credentials(account,user_id,usercode):
+    """
+    creating user credentials
+    """
+    new_credentials=Credentials(account,user_id,usercode)
+    return new_credentials
+
+def save_credentials(credentials):
+    """
+    save credentials
+    """
+    credentials.save_credentials()   
+
+def delete_credentials(credentials):
+    """
+    to delete credentials
+    """
+    credentials.delete_credentials()
