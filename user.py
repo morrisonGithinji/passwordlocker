16db095a56e7b2d18648a50d3d66b1d2257b7a19
morrisonGithinji <morrison.githinji@student.moringaschool.com>
2021-10-26 15:38:08 +0300

    correct identation errors

--- user.py
+++ user.py
@@ -1,23 +1,24 @@
-class User :
-  """
-  class for initiating new users
-  """
-  user_list=[]
-
-def __init__(self, user_name,password):
-  self.userName = user_name
-  self.password =password
-
+class User:
+  '''
+  a class instantiating new users
+  '''
+  
+  user_list = []
+  
+  def __init__(self, user_name, password):
+    self.user_name = user_name
+    self.password = password
+    
   def save_user(self):
-    """
-    saves user in the user_list
-    """
+    '''
+    save user function to save users into the user_list
+    '''
     User.user_list.append(self)
-
+    
   @classmethod
-  def user_exists(cls,user_name,password):
-    default_user =""
+  def user_exists(cls,user_name,password):  
+    default_user = ""
     for user in cls.user_list:
-      if user.user_name ==user_name and user.password ==password:
+      if user.user_name == user_name and user.password == password :
         default_user =user.user_name
         return default_user
